effe3e33df0954c8c7cea8abb292c2a5941bdade
lluisguardiola <lluisguardiola.h@gmail.com>
2021-01-25 17:57:30 -0600

    rounting and tabs

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
+import {createOutline, listCircleOutline, personCircleOutline, searchOutline, trendingUpOutline} from "ionicons/icons"
+import { IonReactRouter } from '@ionic/react-router';
+import Home from './pages/Home';
+import Trending from './pages/Trending';
+import Submit from './pages/Submit';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const App = () => {
+  return (
+    <IonApp>
+      <IonReactRouter>
+      <IonTabs>
+          <IonRouterOutlet>
+            <Route path="/" render={() => <Redirect to="/home" />} exact={true} />
+            <Route path="/home" component={Home} />
+            <Route path="/trending" component={Trending}/>
+            <Route path="/search" component={Search}/>
+            <Route path="/submit" component={Submit}/>
+            <Route path="/profile" component={Profile}/>
+            <Route component={() => <Redirect to="/home"/>} /> 
+          </IonRouterOutlet>
+        
+          <IonTabBar slot="top">
+            <IonTabButton tab="home" href="/home">
+              <IonIcon icon={listCircleOutline} />
+              <IonLabel>Hunt</IonLabel>
+            </IonTabButton>
+
+            <IonTabButton tab="trending" href="/trending">
+              <IonIcon icon={trendingUpOutline} />
+              <IonLabel>Trending</IonLabel>
+            </IonTabButton>
+
+            <IonTabButton tab="submit" href="/submit">
+              <IonIcon icon={createOutline} />
+              <IonLabel>Submit</IonLabel>
+            </IonTabButton>
+
+            <IonTabButton tab="search" href="/search">
+              <IonIcon icon={searchOutline} />
+              <IonLabel>Search</IonLabel>
+            </IonTabButton>
+
+            <IonTabButton tab="profile" href="/profile">
+              <IonIcon icon={personCircleOutline} />
+              <IonLabel>Profile</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+
+      </IonReactRouter>
+    </IonApp>
+  )
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react" 
+
+const Home = () => {
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Hunt</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+		</IonPage>
+	)
+}
+
+export default Home
