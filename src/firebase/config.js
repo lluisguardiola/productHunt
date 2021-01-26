b4b4b1244a1aa53acb012e9a4a6e1c2595410fae
lluisguardiola <lluisguardiola.h@gmail.com>
2021-01-26 13:05:45 -0600

    firebase structure setup

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyByfH7sgDhuLnew6Mr_9R7V-IXr0zgQteo",
+	authDomain: "hunt-30b37.firebaseapp.com",
+	projectId: "hunt-30b37",
+	storageBucket: "hunt-30b37.appspot.com",
+	messagingSenderId: "697234321487",
+	appId: "1:697234321487:web:232f0efd1dedc3c433ba3a"
+}
+
+export default firebaseConfig
