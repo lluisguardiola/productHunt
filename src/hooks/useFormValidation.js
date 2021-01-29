47d644b4d5c7e4d22998726ca992a608ca35a667
lluisguardiola <lluisguardiola.h@gmail.com>
2021-01-28 23:41:11 -0600

    useForm Hook

---
+++ src/hooks/useFormValidation.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import {toast} from '../utils/toast';
+
+const useFormValidation = (initialState, validate, action) => {
+	const [values, setValues] = useState(initialState)
+	const [errors, setErrors] = useState({})
+	const [isSubmitting, setSubmitting] = useState(false)
+
+	useEffect(() => {
+		if (isSubmitting) {
+			const noErrrors = Object.keys(errors).length === 0
+			if (noErrrors) {
+				action()
+				setValues(initialState)
+				setSubmitting(false)
+			} else {
+				toast(Object.values(errors).join(" "))
+				setSubmitting(false)
+			}
+		}
+		// eslint-disable-next-line
+	}, [errors])
+
+	const handleChange = (event) => {
+		setValues(previousValues => ({
+			...previousValues,
+			[event.target.name]: event.target.value
+		}))
+	}
+
+	const handleSubmit = () => {
+		const validationErrors = validate(values)
+		setErrors(validationErrors)
+		setSubmitting(true)
+	}
+
+	return {
+		handleSubmit, handleChange, values, setValues, isSubmitting
+	}
+}
+
+export default useFormValidation
