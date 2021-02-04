9d2c1a5e52d55fe880ad926c4a89b5027f4b22bc
lluisguardiola <lluisguardiola.h@gmail.com>
2021-02-04 11:26:00 -0600

    console warning cleanup

--- src/hooks/useFormValidation.js
+++ src/hooks/useFormValidation.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {toast} from '../utils/toast';
 
 const useFormValidation = (initialState, validate, action) => {
